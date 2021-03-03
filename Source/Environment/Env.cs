3bba75a255efdea6b259f3c9b1663dc7897ed5eb
Sokol <sokol@rfibank.ru>
2021-03-03 17:02:09 +0300

    Добавлен Source/Environment.

---
+++ Source/Environment/Env.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OpenApiAdapter.Source.Environment
+{
+    public static class Env
+    {
+        public static string Get(string name) => System.Environment.GetEnvironmentVariable(name);
+
+        public static readonly bool IsDebug = Debugger.IsAttached;
+
+        public static string OpenApiUri => Get("OPEN_API_URI") ?? throw new Exception("OpenApiUri not set");
+
+        public static string RfiPublicKey => Get("RFI_KEY_PUBLIC") ?? throw new Exception("RfiPublicKey not set");
+
+        public static string PartnerPrivateKey => Get("PARTNER_KEY_PRIVATE") ?? throw new Exception("PartnerPrivateKey not set");
+
+        public static string CertificateFilePath => Get("CERTIFICATE_FILE") ?? throw new Exception("CertificateFilePath not set");
+    }
+}
