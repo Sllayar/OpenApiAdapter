196932cc78d0f9e4ebec9564bcccf9ea87430091
Sokol <sokol@rfibank.ru>
2021-03-03 14:33:02 +0300

    Добавлен роутинг всех путей.

--- Source/Controllers/RetranslateController.cs
+++ Source/Controllers/RetranslateController.cs
@@ -13,7 +13,7 @@ using System.Threading.Tasks;
 
 namespace OpenApiAdapter.Source
 {
-    [Route("adapter/retranslate")]
+    [Route("{*url}")]
     [ApiController]
     public class RetranslateController : ControllerBase
     {
